a9272331a95c4816ac6c36d4645ad694aed59dce
dowenliu-xyz <hawkdowen@hotmail.com>
2019-11-26 18:02:46 +0800

    IntList, NoHeadIntSinglyLinkedList

---
+++ java/src/main/java/xyz/dowenliu/study/algo/_07_linked_list/IntList.java
@@ -0,0 +1,113 @@
+package xyz.dowenliu.study.algo._07_linked_list;
+
+/**
+ * {@code int} 列表
+ * <p>create at 2019/11/26</p>
+ *
+ * @author liufl
+ * @since version 1.0
+ */
+public interface IntList {
+    /**
+     * 返回列表包含的值数量
+     *
+     * @return 列表包含的值数量
+     */
+    int size();
+
+    /**
+     * 列表是否为空
+     *
+     * @return 列表为空，返回 {@code true} ；否则，返回 {@code false}
+     */
+    default boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    /**
+     * 按索引位置获取值
+     *
+     * @param index 索引
+     * @return 索引位置上的值
+     * @throws IndexOutOfBoundsException 索引访问越界
+     */
+    int get(int index) throws IndexOutOfBoundsException;
+
+    /**
+     * 判断列表中是否有对应值
+     *
+     * @param value 要判断的值
+     * @return 包含，返回 {@code true} ；否则，返回 {@code false}
+     */
+    boolean contains(int value);
+
+    /**
+     * 修改索引位置的值
+     *
+     * @param index 索引
+     * @param value 要改为的值
+     * @return 修改前的值
+     * @throws IndexOutOfBoundsException 索引访问越界
+     */
+    int set(int index, int value) throws IndexOutOfBoundsException;
+
+    /**
+     * 在索引位置之前添加值
+     *
+     * @param index 索引
+     * @param value 要添加的值
+     * @throws IndexOutOfBoundsException 索引访问越界
+     */
+    void addBefore(int index, int value) throws IndexOutOfBoundsException;
+
+    /**
+     * 添加值到列表头部
+     *
+     * @param value 要添加的值
+     */
+    default void addHead(int value) {
+        this.addBefore(0, value);
+    }
+
+    /**
+     * 在索引位置之后添加值
+     *
+     * @param index 索引
+     * @param value 要添加的值
+     * @throws IndexOutOfBoundsException 索引访问越界
+     */
+    void addAfter(int index, int value) throws IndexOutOfBoundsException;
+
+    /**
+     * 添加值到列表末尾
+     * @param value 要添加的值
+     */
+    default void addTail(int value) {
+        this.addAfter(this.size() - 1, value);
+    }
+
+    /**
+     * 删除索引位置的值
+     *
+     * @param index 索引
+     * @return 删除前索引位置的值
+     * @throws IndexOutOfBoundsException 索引访问越界
+     */
+    int remove(int index) throws IndexOutOfBoundsException;
+
+    /**
+     * 删除第一个具有指定值的值
+     *
+     * @param value 要删除的值
+     * @return 找到并删除了值，返回 {@code true} ；否则，返回 {@code false}
+     */
+    boolean removeBy(int value);
+
+    int indexOf(int value);
+
+    int lastIndexOf(int value);
+
+    int[] toArray();
+
+    void clear();
+}
---
+++ java/src/main/java/xyz/dowenliu/study/algo/_07_linked_list/NoHeadIntSinglyLinkedList.java
@@ -0,0 +1,258 @@
+package xyz.dowenliu.study.algo._07_linked_list;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * 无头切点 {@code int} 单链表
+ * <p>create at 2019/11/26</p>
+ *
+ * @author liufl
+ * @since version 1.0
+ */
+public class NoHeadIntSinglyLinkedList implements IntList, Serializable {
+    private static class Node implements Serializable {
+        int value;
+        Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    private Node nodeAt(int index) {
+        if (index < 0) {
+            return null;
+        }
+        Node cursor = this.head;
+        int position = 0;
+        while (position != index && cursor != null) {
+            cursor = cursor.next;
+            position++;
+        }
+
+        return cursor;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: " + index + ", Size: " + this.size;
+    }
+
+    @Override
+    public int get(int index) {
+        this.checkIndex(index);
+        Node node = this.nodeAt(index);
+        assert node != null : "index is checked, node should not be null";
+        return node.value;
+    }
+
+    private Node nodeBeforeValue(int value) {
+        Node cursor = this.head;
+        if (cursor == null) {
+            // no node in this list
+            return null;
+        }
+        if (cursor.value == value) {
+            // this node with value is head, and no node is before head.
+            return null;
+        }
+        boolean find = false;
+        while (cursor.next != null) {
+            if (cursor.next.value == value) {
+                find = true;
+                break;
+            }
+            cursor = cursor.next;
+        }
+        return find ? cursor : null;
+    }
+
+    private Node nodeOfValue(int value) {
+        Node beforeValue = this.nodeBeforeValue(value);
+        if (beforeValue == null) {
+            if (this.head != null && this.head.value == value) {
+                return this.head;
+            }
+            return null;
+        }
+        return beforeValue.next;
+    }
+
+    @Override
+    public boolean contains(int value) {
+        return nodeOfValue(value) != null;
+    }
+
+    @Override
+    public int set(int index, int value) {
+        this.checkIndex(index);
+        Node node = this.nodeAt(index);
+        assert node != null : "index is checked, node should not be null";
+        int origin = node.value;
+        node.value = value;
+        return origin;
+    }
+
+    private void addHeader(Node node) {
+        Objects.requireNonNull(node);
+        node.next = head;
+        head = node;
+        this.size++;
+    }
+
+    private void addHeader(int value) {
+        Node node = new Node(value, null);
+        this.addHeader(node);
+    }
+
+    @Override
+    public void addBefore(int index, int value)
+            throws IndexOutOfBoundsException {
+        if (index < 0 || index > this.size) {
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+        if (index == 0) {
+            this.addHeader(value);
+            return;
+        }
+
+        // 1 <= index <= size
+        // 0 <= index - 1 <= size - 1
+        addAfter(index - 1, value);
+    }
+
+    @Override
+    public void addAfter(int index, int value) {
+        this.checkIndex(index);
+        Node node = this.nodeAt(index);
+        assert node != null : "index is checked, node should not be null";
+        node.next = new Node(value, node.next);
+        this.size++;
+    }
+
+    @Override
+    public int remove(int index) {
+        this.checkIndex(index);
+        Node previous = this.nodeAt(index - 1);
+        Node node;
+        if (previous == null) {
+            assert index == 0 : "only head node has no previous node";
+            node = this.head; //
+            assert node != null : "index checking is passed，" +
+                    "so head node, the node at index 0, must exist.";
+            this.head = node.next;
+        } else {
+            node = previous.next;
+            assert node != null : "index checking is passed," +
+                    " so the node at index " + index + " must exist.";
+            previous.next = node.next;
+        }
+        node.next = null; // help GC
+        this.size--;
+        return node.value;
+    }
+
+    @Override
+    public boolean removeBy(int value) {
+        Node previous = this.nodeBeforeValue(value);
+        if (previous != null) {
+            Node node = previous.next;
+            assert node != null;
+            previous.next = node.next;
+            node.next = null; // help GC
+            this.size--;
+            return true;
+        }
+        if (this.head != null && this.head.value == value) {
+            remove(0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int indexOf(int value) {
+        Node cursor = this.head;
+        int position = 0;
+        boolean find = false;
+        while (cursor != null) {
+            if (cursor.value == value) {
+                find = true;
+                break;
+            }
+            cursor = cursor.next;
+            position++;
+        }
+        return find ? position : -1;
+    }
+
+    @Override
+    public int lastIndexOf(int value) {
+        Node cursor = this.head;
+        int position = 0;
+        int lastPosition = -1;
+        while (cursor != null) {
+            if (cursor.value == value) {
+                lastPosition = position;
+            }
+            cursor = cursor.next;
+            position++;
+        }
+        return lastPosition;
+    }
+
+    @Override
+    public int[] toArray() {
+        int[] array = new int[this.size];
+        Node cursor = this.head;
+        int index = 0;
+        while (cursor != null) {
+            array[index] = cursor.value;
+            cursor = cursor.next;
+            index++;
+        }
+        return array;
+    }
+
+    @Override
+    public void clear() {
+        while (this.head != null) {
+            this.remove(0);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof IntList)) return false;
+        IntList that = (IntList) o;
+        return Arrays.equals(this.toArray(), that.toArray());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(this.toArray());
+    }
+
+    @Override
+    public String toString() {
+        return "NoHeadIntSinglyLinkedList[" +
+                Arrays.toString(this.toArray()) + ']';
+    }
+}
