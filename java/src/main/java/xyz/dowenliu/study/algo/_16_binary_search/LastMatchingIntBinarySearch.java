a9c208c8951f35457db95f23ec1e7cae6bb666cc
dowenliu-xyz <hawkdowen@hotmail.com>
2019-12-02 11:33:06 +0800

    LastMatchingIntBinarySearch

---
+++ java/src/main/java/xyz/dowenliu/study/algo/_16_binary_search/LastMatchingIntBinarySearch.java
@@ -0,0 +1,31 @@
+package xyz.dowenliu.study.algo._16_binary_search;
+
+import org.jetbrains.annotations.NotNull;
+import xyz.dowenliu.study.algo._15_binary_search.IntBinarySearch;
+
+/**
+ * 二分查找。查找最后一个值等于给定值的元素
+ * <p>create at 2019/12/2</p>
+ *
+ * @author liufl
+ * @since version 1.0
+ */
+public class LastMatchingIntBinarySearch implements IntBinarySearch {
+    @Override
+    public int search(@NotNull int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (array[mid] == key) {
+                if (mid == array.length - 1 || array[mid + 1] > key) return mid;
+                left = mid + 1;
+            } else if (array[mid] > key) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ java/src/test/java/xyz/dowenliu/study/algo/_16_binary_search/LastMatchingIntBinarySearchTest.java
@@ -0,0 +1,70 @@
+package xyz.dowenliu.study.algo._16_binary_search;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import xyz.dowenliu.study.algo._15_binary_search.IntBinarySearch;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * <p>create at 2019/12/2</p>
+ *
+ * @author liufl
+ * @since version 1.0
+ */
+public class LastMatchingIntBinarySearchTest {
+    private int[] array;
+    private int keyExists;
+    private int keyNotExists;
+
+    @Before
+    public void prepareData() {
+        //*/
+        int length = 100_000;
+        int bound = 10_000;
+        /*/
+        int length = 10;
+        int bound = 5;
+        //*/
+        this.array = new int[length];
+        Random random = new Random(System.currentTimeMillis());
+        Set<Integer> added = new HashSet<>();
+        for (int i = 0; i < this.array.length; i++) {
+            int value = random.nextInt(bound);
+            if (!added.add(value)) {
+                this.keyExists = value;
+            }
+            this.array[i] = value;
+        }
+        Arrays.sort(this.array);
+        this.keyNotExists = -1;
+    }
+
+    @Rule
+    public Timeout timeout = new Timeout(5, TimeUnit.SECONDS);
+
+    @Test
+    public void test() {
+        IntBinarySearch binarySearch = new LastMatchingIntBinarySearch();
+        assertThat(binarySearch.search(this.array, this.keyNotExists)).isEqualTo(-1);
+        int position = binarySearch.search(this.array, this.keyExists);
+        assertThat(position).isGreaterThan(-1);
+        if (position > 0) {
+            assertThat(this.array[position - 1]).isEqualTo(this.array[position]);
+        }
+        if (position < this.array.length) {
+            assertThat(this.array[position]).isLessThan(this.array[position + 1]);
+        } else {
+            System.out.println("The key last appears at index " + position);
+        }
+        assertThat(this.array[position]).isEqualTo(this.keyExists);
+    }
+}
